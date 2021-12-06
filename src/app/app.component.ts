a2c88f089e73dc8314cc76461445720d915bc368
atanas.bochukov <atanas.bochukov@bulpros.com>
2021-12-06 15:19:22 +0200

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faChartLine, faSatellite, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine, faGlobe, faSatellite, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,12 @@ import { faChartLine, faSatellite, faSearch } from '@fortawesome/free-solid-svg-
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wbg';
-  faSearch = faSearch;
   faChartLine = faChartLine;
+  faGlobe = faGlobe;
   faSatellite = faSatellite;
+  faSearch = faSearch;
+  
+  title = 'World Bank';
 
-  public isCollapsed = false;
+  public isCollapsed = true;
 }
