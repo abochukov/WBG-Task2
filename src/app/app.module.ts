b9fae6cb7fa53f8a08947618a89cd60abfa82e4f
atanas.bochukov <atanas.bochukov@bulpros.com>
2021-12-06 11:58:13 +0200

    Add fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     NgbModule
   ],
   providers: [],
